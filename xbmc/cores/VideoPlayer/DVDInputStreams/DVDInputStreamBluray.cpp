ce583c0e8a920ad6214e08e20f8926f299afe5e5
ace20022 <ace20022@ymail.com>
2016-03-23 18:31:28 +0100

    [bluray] Add file_write implementation.

--- xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamBluray.cpp
+++ xbmc/cores/VideoPlayer/DVDInputStreams/DVDInputStreamBluray.cpp
@@ -40,6 +40,7 @@
 #include "settings/DiscSettings.h"
 #include "utils/LangCodeExpander.h"
 #include "filesystem/SpecialProtocol.h"
+#include "utils/StringUtils.h"
 
 #ifdef TARGET_POSIX
 #include "linux/XTimeUtils.h"
@@ -83,7 +84,7 @@ int64_t DllLibbluray::file_read(BD_FILE_H *file, uint8_t *buf, int64_t size)
 
 int64_t DllLibbluray::file_write(BD_FILE_H *file, const uint8_t *buf, int64_t size)
 {
-    return -1;
+  return static_cast<CFile*>(file->internal)->Write(buf, size);
 }
 
 BD_FILE_H * DllLibbluray::file_open(const char* filename, const char *mode)
@@ -98,6 +99,12 @@ BD_FILE_H * DllLibbluray::file_open(const char* filename, const char *mode)
     file->eof   = file_eof;
 
     CFile* fp = new CFile();
+    if (StringUtils::EqualsNoCase(mode, "wb") && fp->OpenForWrite(filename, true))
+    {
+      file->internal = (void*)fp;
+      return file;
+    }
+
     if(fp->Open(filename))
     {
       file->internal = (void*)fp;
